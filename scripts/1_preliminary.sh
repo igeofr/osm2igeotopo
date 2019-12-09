2104f08ee172c12b9badf7a8e608df5bb805bd96
Flø <florian.boret@laposte.net>
2019-12-09 11:17:47 +0100

    Update 1_preliminary.sh

--- scripts/1_preliminary.sh
+++ scripts/1_preliminary.sh
@@ -2,13 +2,13 @@
 #OSM2IGEOTOPO - FRANCE
 ##########################
 
-cd "/data_in/oceans_seas"
+cd "/osm2igeotopo/data_in/oceans_seas"
 rm water-polygons-split-4326.*
 rm -r water-polygons-split-4326
 curl --limit-rate 200K https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip > "./water-polygons-split-4326.zip"
 unzip -o water-polygons-split-4326.zip
 
 ###----------
-cd "/data_in/dsm/"
+cd "/osm2igeotopo/data_in/dsm/"
 ### ATTENTION : Au préalable, vous devez télécharger et décompresser les différentes tuiles EU-DEM dans le dossier dsm
 gdalbuildvrt ./1_VRT.vrt ./*.TIF
